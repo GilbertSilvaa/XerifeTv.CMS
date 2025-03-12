bb84d7ec2621b7785318b15d3a058f852a24883e
gilbertsilvaa <kemegil515@gmail.com>
2025-03-12 14:04:31 -0300

    :bug: fix: while a form is being submitted all its fields and buttons are disabled

--- XerifeTv.CMS/wwwroot/js/site.js
+++ XerifeTv.CMS/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function() {
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach(form => {
+    if (form.method.toUpperCase() === 'POST') {
+      const elements = form.querySelectorAll('input, select, button');
+
+      form.addEventListener('submit', () =>
+        elements.forEach(element => element.disabled = true));
+    }
+  });
+});
