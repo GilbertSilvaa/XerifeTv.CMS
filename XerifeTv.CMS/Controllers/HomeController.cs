f7da81c24af839235978a39fbfc0eabda27e0db5
gilbertsilvaa <kemegil515@gmail.com>
2025-03-18 21:28:02 -0300

    :recycle: refactor: move refresh token validation to UserController and restore intended navigation after session update
    - Removed middleware responsible for refresh token validation
    
    - Added a new route in UserController to handle refresh token validation
    
    - Ensured that after a successful session update, the user is redirected to the originally requested URL before the 401 response

--- XerifeTv.CMS/Controllers/HomeController.cs
+++ XerifeTv.CMS/Controllers/HomeController.cs
@@ -1,19 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using XerifeTv.CMS.Models.Dashboard.Dtos.Response;
 using XerifeTv.CMS.Models.Dashboard.Interfaces;
 
 namespace XerifeTv.CMS.Controllers;
 
+[Authorize]
 public class HomeController(IDashboardService _service, ILogger<HomeController> _logger) : Controller
 {
   public async Task<IActionResult> Index()
   {
-    if (User.Identity is null) 
-      return RedirectToAction("SignIn", "Users");
-
-    if (!User.Identity.IsAuthenticated) 
-      return RedirectToAction("SignIn", "Users");
-
     var response = await _service.Get();
 
     _logger.LogInformation($"{User.Identity.Name} accessed the dashboard page");
