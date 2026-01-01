2d1c611834aaf275747eacf85714f7c8d0042af5
gilbertsilvaa <kemegil515@gmail.com>
2026-01-01 18:54:28 -0300

    :recycle: refactor(webhooks): make webhook dispatch resilient with retries

--- XerifeTv.CMS/Modules/Common/Enums/EHttpMethod.cs
+++ XerifeTv.CMS/Modules/Common/Enums/EHttpMethod.cs
@@ -8,3 +8,26 @@ public enum EHttpMethod
     PATCH = 4,
     DELETE = 5
 }
+
+public static class EHttpMethodExtensions
+{
+    public static HttpMethod ToHttpMethod(this EHttpMethod method) => method switch
+    {
+        EHttpMethod.POST => HttpMethod.Post,
+        EHttpMethod.GET => HttpMethod.Get,
+        EHttpMethod.PUT => HttpMethod.Put,
+        EHttpMethod.PATCH => HttpMethod.Patch,
+        EHttpMethod.DELETE => HttpMethod.Delete,
+        _ => throw new ArgumentOutOfRangeException(nameof(method), method, null)
+    };
+
+    public static bool IsBodySupported(this EHttpMethod method) => method switch
+    {
+        EHttpMethod.POST => true,
+        EHttpMethod.PUT => true,
+        EHttpMethod.PATCH => true,
+        EHttpMethod.GET => false,
+        EHttpMethod.DELETE => false,
+        _ => throw new ArgumentOutOfRangeException(nameof(method), method, null)
+    };
+}
